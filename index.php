103d9879ac9c3d8b1e27a3d57cd08180ccbb8b2c
GIOVANA ALVES FRADE <25170915@educ123.sp.senai.br>
2025-08-12 16:58:56 -0300

    atualizações

--- index.php
+++ index.php
@@ -62,7 +62,6 @@
                                 echo 'Arquivo-fonte txt não encontrado!';
                             }
                         ?>
-                        <!-- Há 6 anos envolvida na área de programação e amando cada vez mais a área. Busco por uma oportunidade profissional que me permita aplicar meus conhecimentos, desenvolver novas habilidades e contribuir com os resultados da empresa. Tenho interesse em atuar como desenvolvedora FullStack. Estou disposta à aprender, colaborar em equipe e assumir novos desafios. -->
                     </p>
                 </div>
                 <div class="sobre">
@@ -86,40 +85,35 @@
             <h1>sobre</h1>
             <div class="tela-sobre">
                 <div class="foto-sobre">
-                    <img src="media/mulher.png">
+                    <img src="media/fotosobre.jpeg"> 
                 </div>
                 <div class="texto-sobre">
                     <p>
                         <?php 
-                            // Nome do arquivo de texto
                             $textosobre = 'media/informacoestxt/textosobre.txt';
 
-                            // Verifica se o arquivo existe
                             if (file_exists($textosobre)) {
-                                // Lê todo o conteúdo do arquivo para uma string
                                 $textosobre = file_get_contents($textosobre);
-                                // Exibe o conteúdo dentro do elemento
                                 echo $textosobre;
                             } else{
                                 echo 'Arquivo-fonte txt não encontrado!';
                             }
                         ?>
-                        <!-- Meu nome é Giovana Frade, tenho 16 anos e sou de São Paulo. Atualmente, estou no 2º ano do Ensino Médio e estou cursando o Técnico de Desenvolvimento de Sistemas pelo SENAI Armando de Arruda Pereira. Além disso, sou certificada pelo SENAI de Informática "Paulo Antonio Skaf" nos seguintes cursos:  -->
-
                     </p>
                     <ul>
-                        <li>Microsoft AI-102: Desenvolvimento de Soluções em Inteligência Artificial (Maio – Junho de 2025,
-                            60 horas)</li>
-                        <li>Microsoft AI-900: Implantação de Serviços de Inteligência Artificial em Nuvem (Fevereiro de
-                            2025, 40 horas)</li>
-                        <li>Excel Básico (Março – Abril de 2025, 20 horas)</li>
-                        <li>Ética na Inteligência Artificial (Março – Abril de 2025, 4 horas) </li>
-                        <li>Segurança no Trabalho (Março – Abril de 2025, 14 horas)</li>
-                        <li>Fluência - Fundamentos da Inteligência Artificial (Janeiro – Fevereiro de 2025, 8 horas)</li>
-                        <li>Fotografia Digital (Janeiro – Fevereiro de 2025, 40 horas)</li>
-                        <li>Design UX e UI (Novembro – Dezembro de 2024, 40 horas)</li>
-                        <li>Microsoft Power-BI (Outubro – Novembro de 2024, 32 horas)</li>
-                        <li>Microsoft PL-500: Desenvolvedor de RPA Microsoft Power Automate (Outubro – Novembro de 2024, 32 horas)</li>
+                        <?php
+                            $arquivo = file('media/informacoestxt/lista.txt');
+                            echo '<li>' . $arquivo[0] . '</li>';
+                            echo '<li>' . $arquivo[1] . '</li>';
+                            echo '<li>' . $arquivo[2] . '</li>';
+                            echo '<li>' . $arquivo[3] . '</li>';
+                            echo '<li>' . $arquivo[4] . '</li>';
+                            echo '<li>' . $arquivo[5] . '</li>';
+                            echo '<li>' . $arquivo[6] . '</li>';
+                            echo '<li>' . $arquivo[7] . '</li>';
+                            echo '<li>' . $arquivo[8] . '</li>';
+                            echo '<li>' . $arquivo[9] . '</li>';
+                        ?>
                     </ul>
                     <h1>tecnologias</h1>
                     <div class="tecnologias">
@@ -164,27 +158,19 @@
                         <h2>Projeto 1 - Jogos Vorazes</h2>
                         <p>
                             <?php 
-                                // Nome do arquivo de texto
-                                   $textoprojeto1 = 'media/informacoestxt/projeto1.txt';
+                                $textoprojeto1 = 'media/informacoestxt/projeto1.txt';
 
-                                // Verifica se o arquivo existe antes de tentar ler
                                 if (file_exists($textoprojeto1)) {
-                                    // Lê todo o conteúdo do arquivo para uma string
                                     $textoprojeto1 = file_get_contents($textoprojeto1);
-                                    // Exibe o conteúdo dentro do elemento
                                     echo $textoprojeto1;
                                 } else{
                                     echo 'Arquivo-fonte txt não encontrado!';
                                 }
                             ?>
-                            <!-- O projeto consiste em um website em que contém as informações gerais 
-                                sobre cada filme da saga Jogos Vorazes, inspirado nos livros de 
-                                Suzanne Collins, como ficha técnica, uma breve sinopse e o trailer.
-                                A finalidade é induzir novos espectadores a consumir a saga de filmes. -->
                         </p>
                         <div class="tecnologias-projeto">
-                            <i class="fa-brands fa-html5 fa-2xl" id="html-icon"></i>
-                            <i class="fa-brands fa-css3-alt fa-2xl" id="css-icon"></i>
+                            <i class="fa-brands fa-html5 fa-2xl" id="html-icon" aria-label="HTML"></i>
+                            <i class="fa-brands fa-css3-alt fa-2xl" id="css-icon" aria-label="CSS"></i>
                         </div>
                         <div class="botao-versite">
                             <div class="botao">
@@ -202,30 +188,21 @@
                         <h2>Projeto 2 - Sprout</h2>
                         <p>
                             <?php 
-                                // Nome do arquivo de texto
                                 $textoprojeto2 = 'media/informacoestxt/projeto2.txt';
 
-                                // Verifica se o arquivo existe antes de tentar ler
                                 if (file_exists($textoprojeto2)) {
-                                    // Lê todo o conteúdo do arquivo para uma string
                                     $textoprojeto2 = file_get_contents($textoprojeto2);
-                                   // Exibe o conteúdo dentro do elemento
                                     echo $textoprojeto2;
                                 } else{
                                     echo 'Arquivo-fonte txt não encontrado!';
                                 }
                             ?>
-                            <!-- O projeto consiste em um sistema em que o usuário poderá criar contatos e gerenciá-los,
-                            criando grupos e adicionando contatos a eles. O utilizador do site também tem a opção
-                            de mudar a cor-tema do site, alterando para cor de sua preferência, pois o sistema
-                            prioriza a entrega de um sistema de qualidade ao usuário e que vá de acordo com o gosto do
-                            usuário. -->
                         </p>
                         <div class="tecnologias-projeto">
-                            <i class="fa-brands fa-html5 fa-2xl" id="html-icon"></i>
-                            <i class="fa-brands fa-css3-alt fa-2xl" id="css-icon"></i>
-                            <i class="fa-brands fa-square-js fa-2xl" id="js-icon"></i>
-                            <i class="fa-brands fa-regular fa-php fa-2xl" id="php-icon"></i>
+                            <i class="fa-brands fa-square-js fa-2xl" id="js-icon" aria-label="JAVASCRIPT"></i>
+                            <i class="fa-brands fa-html5 fa-2xl" id="html-icon" aria-label="HTML"></i>
+                            <i class="fa-brands fa-css3-alt fa-2xl" id="css-icon" aria-label="CSS"></i>
+                            <i class="fa-brands fa-regular fa-php fa-2xl" id="php-icon" aria-label="PHP"></i>
                         </div>
                         <div class="botao-versite">
                             <div class="botao">
